8a00a900c852f8497d22e7346acfc99c4c9d62a8
Bayron José Galo Espinal <91099827+Bgalo@users.noreply.github.com>
2026-08-08 18:54:45 -0600

    Update salida.js

--- cantos/salida.js
+++ cantos/salida.js
@@ -34,6 +34,55 @@ No lo hay, no lo hay (2).
 
 No hay Dios
 que pueda hacer las cosas
-como las que haces Tú. (2)`}
+como las que haces Tú. (2)`} 
+    
+    {
+      titulo: "Oración por la familia", tono: "ReM",
+      letra: `Que ninguna familia comience
+en cualquier de repente.
+Que ninguna familia se acabe
+por falta de amor.
+La pareja sea el uno en el otro
+de cuerpo y de mente,
+y que nada en el mundo
+separe un hogar soñador.
+Que ninguna familia
+se albergue debajo del puente,
+y que nadie interfiera en la vida
+y en la paz de los dos,
+y que nadie los haga vivir
+sin ningún horizonte,
+y que puedan vivir
+sin temer lo que venga después.
+
+La familia comience sabiendo
+por qué y dónde va, y que el hombre
+retrate la gracia de ser un papá.
+La mujer sea cielo y ternura,
+y afecto y calor,
+y los hijos conozcan la fuerza
+que tiene el amor.
+
+Bendecid, oh Señor,
+las familias, Amén.
+Bendecid, oh Señor,
+la mía también. (2)
+
+Que marido y mujer tengan fuerzas
+de amar sin medida.
+y que nadie se vaya a dormir
+sin buscar el perdón.
+En la cuna, los niños aprendan
+el don de la vida,
+la familia celebre el milagro
+del beso y del pan.
+Que marido y mujer, de rodillas,
+contemplen sus hijos,
+que por ellos encuentren
+la fuerza de continuar,
+y que en su firmamento,
+la estrella que tenga más brillo,
+pueda ser la esperanza de paz
+y certeza de amar.`},
 
 ];
