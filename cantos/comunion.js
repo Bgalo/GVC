5a77d87ef9131f2967510fa2ab04b6a2b50ead0f
Bayron José Galo Espinal <91099827+Bgalo@users.noreply.github.com>
2026-08-08 18:58:06 -0600

    Update comunion.js

--- cantos/comunion.js
+++ cantos/comunion.js
@@ -12,7 +12,7 @@ llega en silencio a mí,
 y mis hermanos me hablan de Ti.
 Sé que a mi lado estás,
 te sientas junto a mí:
-Acoges mi vida y mi oración.` }
+Acoges mi vida y mi oración.` },
 
 {titulo: "Oración por la familia", tono: "ReM",
       letra: `Que ninguna familia comience
@@ -60,6 +60,6 @@ la fuerza de continuar,
 y que en su firmamento,
 la estrella que tenga más brillo,
 pueda ser la esperanza de paz
-y certeza de amar.`},
+y certeza de amar.`}
     
 ];
