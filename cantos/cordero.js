4fcffd5157b6866643b481fbbb208bfbc7004cec
Bayron José Galo Espinal <91099827+Bgalo@users.noreply.github.com>
2026-08-25 17:02:18 -0600

    Fix formatting and add Cordero de Dios lyrics
    Agregué el cordero de Dios No.2

--- cantos/cordero.js
+++ cantos/cordero.js
@@ -13,5 +13,17 @@ Señor, ten piedad.
 
 Cordero de Dios,
 tu que quitas el pecado del mundo (2)
-Señor. danos la paz. (3)` }
+Señor. danos la paz. (3)` },
+
+    {
+      id: "C-2",
+      titulo: "Cordero de Dios", tono: "Fa#m",
+      letra: `Cordero de Dios,
+que quitas el pecado del mundo,
+ten piedad de nosotros. (2)
+
+Cordero de Dios que quitas
+el pecado del mundo,
+danos la paz, danos la paz.
+Cordero de Dios.` }
 ];
