1d90622e0996dbb4e5378326ab511530cb5a30e6
Bayron José Galo Espinal <91099827+Bgalo@users.noreply.github.com>
2026-08-25 18:29:50 -0600

    Update comunion.js
    18 nuevos cantos de comunión

--- cantos/comunion.js
+++ cantos/comunion.js
@@ -62,6 +62,317 @@ la fuerza de continuar,
 y que en su firmamento,
 la estrella que tenga más brillo,
 pueda ser la esperanza de paz
-y certeza de amar.`}
-    
+y certeza de amar.`},
+
+    {id: "CM-3",
+      titulo: "Renuevame", tono: "LaM",
+      letra: `Renuévame Señor Jesús
+ya no quiero ser igual
+renuévame Señor Jesús
+pon en mi tu corazón.
+Porque todo lo que hay
+dentro de mi
+necesita ser cambiado
+Señor.
+Porque todo lo que hay
+dentro de mí corazón
+necesita más de ti (bis)`},
+
+     {id: "CM-4",
+      titulo: "En su mesa hay amor", tono: "ReM",
+      letra: `El Señor nos ha reunido junto a Él,
+el Señor nos ha invitado a estar con Él.
+
+En su mesa hay amor,
+la promesa del perdón,
+y en el vino y pan, su corazón.
+En su mesa hay amor,
+la promesa del perdón,
+y en el vino y pan, su corazón.
+
+Cuando, Señor, tu voz,
+llega en silencio a mí,
+y mis hermanos me hablan de Ti,
+sé que a mi lado estás,
+te sientas junto a mí,
+acoges mi vida y mi oración.
+
+El Señor nos ha reunido junto a Él...`},
+
+    {id: "CM-5",
+      titulo: "Pacto de esperanza", tono: "ReM",
+      letra: `Me sorprende Señor
+hallarte tan pequeño
+en un humilde pan.
+Que mi boca pueda triturar
+y poder tener dentro de mí
+a un amigo de verdad.
+
+Me sorprende Señor
+que siendo tú el más grande
+puedas ahí estar.
+Demostrando toda tu humildad,
+Demostrando cuánto eres capaz de amar.
+
+Hoy me alegra Señor saberme convidado
+a tu celebración, sentarme aquí en tu mesa
+y de corazón. Sellar contigo el pacto de esperanza,
+de llevar a los demás tu Pan de Vida,
+y llenar el corazón que triste está. (2)
+`},
+
+    {id: "CM-6",
+      titulo: "Una espiga dorada", tono: "SolM",
+      letra: `Una espiga dorada por el sol,
+el racimo que corta el viñador,
+se convierten ahora en pan
+y vino de amor
+en el cuerpo y la Sangre del Señor. (2)
+
+Comulgamos la misma comunión.
+Somos trigo del mismo sembrador.
+Un molino, la vida, nos tritura con
+dolor.
+Dios nos hace Eucaristía en el amor. (2)
+
+Como granos que han hecho
+el mismo pan,
+como notas que tejen un cantar, como
+gotas de agua
+que se funden en el mar,
+los cristianos un cuerpo formarán. (2)
+`},
+
+    {id: "CM-7",
+      titulo: "Descubrir", tono: "DoM",
+      letra: `Qué hermoso es descubrir,
+cuanto amor tienes por mí. (2)
+
+Qué hermoso es darse cuenta,
+cuanto amor tienes por mí.
+Qué hermoso es darse cuenta
+que Tú me amas muriendo por mí.
+`},
+
+{id: "CM-8",
+      titulo: "Yo te adoro", tono: "MiM",
+      letra: `He podido comprender
+Que solo con tu poder
+Puedo las pruebas vencer, Señor
+Y Tú siempre has sido fiel
+Me has sabido sostener
+Tu brazo me ha dado fuerzas, mi Dios.
+
+Cuando fijas tu mirada
+Puedo ver con claridad
+Que no hay nada en el camino
+Que me impida continuar.
+
+Y yo te adoro, yo te doy toda mi vida
+Te entrego todo, por entero y sin medidas
+Desde el fondo de mi alma
+Cada fibra de mi ser
+Yo te exalto porque siempre has sido fiel (2)
+`},
+
+    {id: "CM-9",
+      titulo: "Pescador (Voy navegando)", tono: "ReM",
+      letra: `Voy navegando sin timón, en mar abierto,
+me abandona la razón.
+Apenas si sobrevivo como un niño perdido,
+busco algo, que no hay en mi interior.
+Más de repente llegas Tú,
+y en tu palabra, el faro de la blanca luz.
+Llévame a puerto seguro, donde hay un futuro,
+donde exista un cielo más azul.
+
+Tu palabra es la esperanza
+que buscamos tantas almas.
+Pescador, tú serás el viento nuevo.
+Tú serás el amigo que nos lleve a un mundo nuevo
+en tu gran corazón infinito. (2)
+
+Si cuesta a veces continuar, en el naufragio
+y entre tanta oscuridad, en medio de un mar 
+que calla y la fe que nos falta voy en busca,
+de un poco de paz.
+Más de repente llegas Tu, y en tu palabra,
+el faro de la blanca luz.
+Llévame a puerto seguro, donde hay un futuro,
+donde exista un cielo más azul.
+`},
+
+    {id: "CM-10",
+      titulo: "Pescador de hombres", tono: "ReM",
+      letra: `Tú has venido a la orilla,
+no has buscado ni a sabios ni a ricos;
+tan sólo quieres que yo te siga.
+
+Señor me has mirado a los ojos,
+sonriendo, has dicho mi nombre,
+en la arena he dejado mi barca,
+junto a ti buscaré otro mar.
+
+Tú sabes bien lo que tengo;
+en mi barca no hay oro ni espadas,
+tan sólo redes y mi trabajo.
+
+Tú necesitas mis manos,
+mi cansancio que a otros descanse,
+amor que quiera seguir amando.
+
+Tú, pescador de otros lagos,
+ansia eterna de almas que esperan,
+amigo bueno que así me llamas.
+`},
+
+    {id: "CM-11",
+      titulo: "Lo más grande", tono: "SolM",
+      letra: `Lo más grande, lo más sublime (2)
+Jesús Eucaristía (2).
+
+Su Cuerpo, su Sangre
+su presencia viva.
+
+Su alma, su Espíritu
+su presencia viva.`},
+
+    {id: "CM-12",
+      titulo: "Pequeño", tono: "ReM",
+      letra: `Pequeño como brisa suave,
+sencillo como una sonrisa niña
+Precioso es tu nombre y tu corazón,
+eterno y profundo es tu amor.
+
+Cristo Jesús, Yo te adoro
+Cristo Jesús, Eterno
+y Profundo es tu Amor.`},
+
+    {id: "CM-13",
+      titulo: "Yo creo en las promesas de Dios", tono: "FaM",
+      letra: `Yo creo en las promesas de Dios (2),
+yo creo en las promesas de mi Señor.
+
+Si soy fiel en lo poco,
+El me confiará más,
+sí soy fiel en lo poco,
+mis pasos guiará.
+
+Yo creo en la misericordia de Dios...
+Yo creo en la Palabra de Dios...
+Yo creo en el amor de Dios...`},
+
+    {id: "CM-14",
+      titulo: "Ángeles en adoración", tono: "DoM",
+      letra: `Se oye un ruido a nuestro alrededor,
+los ángeles llegando para recibir,
+nuestras oraciones y llevarlas Dios;
+si tú el corazón abres y empiezas a alabar
+y sigues orando así vas a notar,
+los ángeles suben y te llevan a Dios.
+
+Hay ángeles volando en este lugar
+en medio del incienso, encima del
+altar, subiendo y bajando en todas
+las direcciones.
+No sé, no sé, si la Iglesia subió o el
+cielo bajo; solo sé que está lleno de
+ángeles de Dios porque el propio
+Dios está aquí.
+
+Cuando los ángeles pasan,
+nuestra Iglesia se alegra, ella canta,
+ella llora, ella ríe y congrega;
+apaga el infierno y disipa el mal.
+Siente el viento de las alas ahora,
+confía hermano que ha llegado la
+hora, esa bendición se derrama ya.`},
+
+    {id: "CM-15",
+      titulo: "Ya no eres pan y vino", tono: "ReM",
+      letra: `Ya no eres pan y vino, ahora que eres cuerpo y sangre,
+vives en mí, de rodillas yo caigo al contemplar 
+tu bondad, como no te voy a adorar.
+
+Mientras te pierdes en mis labios, tu gracia va inundando 
+todo mi corazón por esa paz que me llena
+de alegría mi ser, como no te voy a adorar.
+
+Señor Jesús, mi salvador,
+amor eterno, amor divino
+ya no falta nada, lo tengo todo, te
+tengo a ti (bis)
+
+Dueño y Rey del universo como puede ser posible 
+que busques mi amor. Tú tan grande y yo pequeño
+y te fijas en mí, como no te voy a adorar.
+
+De rodillas yo te pido que el día cuando tú me llames 
+Sea como hoy para mirarte a los ojos y
+poderte decir, que como no te voy a adorar.`},
+
+    {id: "CM-16",
+      titulo: "Háblame", tono: "MiM",
+      letra: `Yo siento, Señor, que Tú me amas,
+yo siento Señor, que te puedo amar.
+Háblame, Señor, que tu siervo escucha,
+háblame, ¿qué quieres de mí?
+
+Señor, Tú has sido grande para mí,
+en el desierto de mi vida; ¡háblame!
+Yo quiero estar dispuesto a todo,
+toma mi ser; mi corazón es para Ti
+Por eso canto tus maravillas,
+por eso canto tu amor. (2)
+
+Te alabo Jesús, por tu grandeza;
+mil gracias te doy por tu gran amor,
+heme aquí, señor, para acompañarte,
+heme aquí; ¿qué quieres de mí?`},
+
+    {id: "CM-17",
+      titulo: "Me has seducido Señor", tono: "Fa#M",
+      letra: `Señor, no soy nada
+¿por qué me has llamado?
+Has pasado por mi puerta y bien
+sabes que soy pobre y soy débil
+¿por qué te has fijado en mí?
+
+Me has seducido, Señor,
+con tu mirada, me has hablado
+al corazón y me has querido.
+Es imposible conocerte y no
+amarte, es imposible amarte y no seguirte,
+Me has seducido, Señor.
+
+Señor, yo te sigo y quiero darte lo que pides,
+aunque hay veces que me cuesta
+darlo todo, tú lo sabes, yo soy tuyo
+camina Señor junto a mí.
+
+Señor, hoy tu nombre,
+Es más que una palabra;
+Es tu voz que hoy resuena en mi
+interior, y me hablas en el silencio,
+¿Qué quieres que haga por ti?`},
+
+    {id: "CM-18",
+      titulo: "Hoy en oración", tono: "ReM",
+      letra: `Hoy en oración, quiero preguntar Señor,
+quiero escuchar tu voz, tu Palabra con tu amor.
+Ser como eres tú, servidor de los demás,
+dime cómo y en qué lugar te hago falta más.
+
+Dime Señor en que te puedo servir,
+déjame conocer tu voluntad.
+Dime Señor en ti yo quiero vivir,
+quiero aprender de ti, saber amar.
+
+Hoy quiero seguir, tu camino junto al mar,
+tu Palabra, tu verdad, ser imagen de ti.
+Ser como eres Tú, servidor de los demás,
+dime cómo y en qué lugar te hago falta más.
+`}
+
+     
 ];
